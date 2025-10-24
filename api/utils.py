39fbbb0d43098f10c8a4f0ef0c6f32aed2e21cfa
Miyawn <tengku.rayhan26@gmail.com>
2025-10-24 15:13:28 +0800

    Clear

--- api/utils.py
+++
@@ -1,6 +0,0 @@
-def get_uptime(start_time):
-    import time
-    seconds = int(time.time() - start_time)
-    m, s = divmod(seconds, 60)
-    h, m = divmod(m, 60)
-    return f"{h:02d}:{m:02d}:{s:02d}"
